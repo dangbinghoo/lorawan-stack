ff3979b2cdef7dcbf96502eb1ff21b5e225e5c62
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-03-25 10:18:23 +0200

    console,oauth: Fix passing of original props

--- pkg/webui/components/field/index.js
+++ pkg/webui/components/field/index.js
@@ -192,16 +192,16 @@ const Field = function (props) {
     rest.value = _value
     rest.onChange = handleChange
     rest.onBlur = handleBlur
-
-    // restore the rest object for future per component filtering
-    rest.name = name
-    rest.readOnly = readOnly
-    rest.disabled = disabled
-    rest.error = _touched && Boolean(_error)
-    rest.warning = Boolean(warning)
-    rest.type = type
-    rest.placeholder = placeholder ? formatMessage(placeholder) : ''
   }
+
+  // restore the rest object for future per component filtering
+  rest.name = name
+  rest.readOnly = readOnly
+  rest.disabled = disabled
+  rest.error = _touched && Boolean(_error)
+  rest.warning = Boolean(warning)
+  rest.type = type
+  rest.placeholder = placeholder ? formatMessage(placeholder) : ''
 
   const hasMessages = _touched && (_error || warning)
 
