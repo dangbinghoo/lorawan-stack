e2f2bb8e8eb06d72e5d2ac140ed99953ba94e28a
Bogdans Afonins <bogdans.afonins@gmail.com>
2019-04-01 16:09:14 +0200

    console: Extend table component add handlePagination prop

--- pkg/webui/console/containers/fetch-table/index.js
+++ pkg/webui/console/containers/fetch-table/index.js
@@ -185,6 +185,7 @@ class FetchTable extends Component {
       headers,
       tabs,
       searchable,
+      handlePagination,
     } = this.props
     const { page, query, tab } = this.state
 
@@ -236,6 +237,7 @@ class FetchTable extends Component {
           headers={headers}
           data={items}
           emptyMessage={sharedMessages.noMatch}
+          handlePagination={handlePagination}
         />
       </div>
     )
